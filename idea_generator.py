8ccabfdf45ae86e079816cb53b8f98843dccf511
Alexandra Kim <alexandrasuriya@gmail.com>
2023-01-14 14:25:47 -0800

    Added GPT ideas to js script

--- idea_generator.py
+++ idea_generator.py
@@ -6,6 +6,7 @@ with open('formattedGPTideas.txt',encoding='utf-8') as file:
     for line in file:
         lines.append(line.rstrip())
         
+print(lines)
 ideas = np.asarray(lines)
 
 generated_ideas = np.random.choice(ideas, 3)
